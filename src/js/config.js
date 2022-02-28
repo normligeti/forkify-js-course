7755b5efa4121a31c24cc38d3596e65d9dd7d31d
normligeti <n.ligeti@gmail.com>
2022-02-28 15:27:35 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ddae9a4d-18c8-43e7-a7e4-1df5135d3e39';
+export const MODAL_CLOSE_SEC = 2.5;
